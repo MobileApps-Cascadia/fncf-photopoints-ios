bd1ac1def9f03a6e0acd63be2def33d1ad5705c1
Akohler28 <43833115+Akohler28@users.noreply.github.com>
2019-04-24 18:09:22 -0700

    Initial Commit
    Initial Commit for Issue1

---
+++ FNCF-PhotoPoints/FNCF-PhotoPoints/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FNCF-PhotoPoints
+//
+//  Created by Student Account on 4/23/19.
+//  Copyright © 2019 Cascadia-Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
