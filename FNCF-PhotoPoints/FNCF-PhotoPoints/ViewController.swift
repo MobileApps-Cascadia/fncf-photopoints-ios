c96770fd67cb20b1d1ddb0d618c8256271af2b72
Aaron <aaronakohler@gmail.com>
2019-04-25 21:26:58 -0700

    Initial Commit for QR Scanner

--- FNCF-PhotoPoints/FNCF-PhotoPoints/ViewController.swift
+++ FNCF-PhotoPoints/FNCF-PhotoPoints/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
