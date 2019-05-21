5e5e48f5d78363fbf32694d7f17910de0ffa068c
Aaron <aaronakohler@gmail.com>
2019-05-20 23:14:43 -0700

    Added segue from scanner to plantInfo on successful scan and plant retrieval. Requires testing on physical device.

--- FNCF-PhotoPoints/FNCF-PhotoPoints/HomeViewController.swift
+++ FNCF-PhotoPoints/FNCF-PhotoPoints/HomeViewController.swift
@@ -8,26 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     //var userInfoCollected = false
     
-   
+ 
     
+    let pm = PlantManager()
+    let testPlant = Plant(plantID: 42, name: "Test", latinName: "tEsT", desc: "This is the desction test, This is the desction test., This is the desction test., This is the desction test., This is the desction test., This is the desction test.This is the desction testThis is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test.This is the desction test..")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        
-        let pm = PlantManager()
         
         print(pm.getPlantByName(name: "Douglas Fir").name)
         print(pm.getPlantByID(id: 2).latinName)
         print(pm.getPlantByID(id: 4).desc)
         print(pm.getPlantByID(id: 5).desc)
+        
+        }
     }
-
+    
+    
     @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
         dismiss(animated: true, completion: nil)
     }
