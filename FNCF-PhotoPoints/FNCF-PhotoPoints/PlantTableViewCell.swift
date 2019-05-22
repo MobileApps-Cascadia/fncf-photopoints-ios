49f12dfb7b38ebeb753fbe416fe188a3f187750d
Aaron <aaronakohler@gmail.com>
2019-05-21 18:04:20 -0700

    Complete #27. UiTableView for plant list with segue to plant info

---
+++ FNCF-PhotoPoints/FNCF-PhotoPoints/PlantTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PlantTableViewCell.swift
+//  FNCF-PhotoPoints
+//
+//  Created by Student Account on 5/21/19.
+//  Copyright © 2019 Cascadia-Mobile. All rights reserved.
+//
+
+import UIKit
+
+class PlantTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var plantNameLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
