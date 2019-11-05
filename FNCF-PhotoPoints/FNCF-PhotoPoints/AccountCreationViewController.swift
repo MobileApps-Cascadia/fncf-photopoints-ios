a30157f34e508915cc2483a5e305bd7ef33060c3
Cascadia.Mobas.EmilioDiaz <ediaz89@uw.edu>
2019-11-05 10:52:58 -0800

    Created modal pop-up for account creation

---
+++ FNCF-PhotoPoints/FNCF-PhotoPoints/AccountCreationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AccountCreationViewController.swift
+//  FNCF-PhotoPoints
+//
+//  Created by Student Account on 11/5/19.
+//  Copyright © 2019 Cascadia-Mobile. All rights reserved.
+//
+
+import UIKit
+
+class AccountCreationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismiss(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func ACSubmit(_ sender: Any) {
+        //Put logic here
+        dismiss(animated: true, completion: nil)
+    }
+}
